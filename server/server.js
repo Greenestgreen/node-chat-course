d193e8e7f6b1c1c0492e3c898c6e9dd654742350
Roberto Alonso Matus John <regular.greener.grass@hotmail.com>
2018-10-29 10:27:57 -0300

    Added connect disconnect events IO

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 console.log(__dirname + '/../public');
@@ -7,9 +9,19 @@ console.log(publicPath);
 
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,() => {
-  console.log('Server is up on port 3000');
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port,() => {
+  console.log(`Server is up on port ${port}`);
 });
