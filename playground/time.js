23ba86b0c05c02e5d581d30e7b0dfc0b58a1aaf5
Roberto Alonso Matus John <regular.greener.grass@hotmail.com>
2018-11-05 10:53:43 -0300

    Added moment for timestamps

---
+++ playground/time.js
@@ -0,0 +1,9 @@
+
+var moment = require('moment');
+
+// var date = new Date();
+// console.log(date.getMonth());
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('k:mm a'));
